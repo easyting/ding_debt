28bf10d34fad20bbdc34b30b62951ce560ec92ce
Giannis Chionidis <gc@inlead.dk>
2013-06-03 14:54:00 +0200

    case 2883 - Removing duplicate ids from sticky tables.

---
+++ js/tableheader_duplicate_id_fix.js
@@ -0,0 +1,13 @@
+ (function($) {
+  Drupal.behaviors.dingDebtTableHeaderFix = {
+    attach: function(context, settings) {
+      // Changing the ids of the header elements, of the sticky-header tables, in the debt_page to avoid duplicates.
+      var mainElement = '.sticky-header';
+      $(mainElement).each(function() {
+        $(this).find('th').each(function() {
+          $(this).attr('id', $(this).attr('id') + '-debt');
+        });
+      });
+    }
+  };   
+})(jQuery);
