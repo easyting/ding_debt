28f129e44043667358591b4bed2f933edf8e9733
Giannis Chionidis <gc@inlead.dk>
2013-06-11 17:30:29 +0200

    case 2883 - Adding missing headers, removing spaces and changing js load.

--- js/ding_debts.js
+++ js/ding_debts.js
@@ -3,11 +3,18 @@
     attach: function(context, settings) {
       // Changing the ids of the header elements, of the sticky-header tables, in the debt_page to avoid duplicates.
       var mainElement = '.sticky-header';
-      $(mainElement).each(function() {
-        $(this).find('th').each(function() {
-          $(this).attr('id', $(this).attr('id') + '-sticky');
-        });
-      });
+      var i = setInterval(function () {
+        // Check if sticky headers are inserted.
+        if ($(mainElement).length) {
+          // Release setInterval after it has entered once.
+          clearInterval(i);
+          $(mainElement).each(function() {
+            $(this).find('th').each(function() {
+              $(this).attr('id', $(this).attr('id') + '-sticky');
+            });
+          });
+        }
+      }, 100);
     }
-  };   
+  };
 })(jQuery);
